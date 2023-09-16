9d446f8a418d6dcb7a17c8403fb527e90a7aac5d
Naqibullah <naqibullah1400@gmail.com>
2023-09-16 15:44:43 +0430

    fix amount descrease problem

--- controllers/forms/ParentBabController.js
+++ controllers/forms/ParentBabController.js
@@ -1,6 +1,48 @@
+// import ParentBabs from './../../models/forms/ParentBab.js';
+// import Appropriations from './../../models/forms/budget/Appropriations.js';
+// import Program from './../../models/forms/budget/Program.js';
+
+// //! Create Parent Bab
+// export const createParentBab = async (req, res) => {
+
+//   const name = req.body.name;
+//   const desc = req.body.desc;
+//   const kind_of = req.body.kind_of;
+//   const kind_of_budget = req.body.kind_of_budget;
+//   const amount = req.body.amount;
+
+//   try {
+//     const data = await ParentBabs.create({
+//       name: name,
+//       desc: desc,
+//       kind_of: kind_of,
+//       kind_of_budget: kind_of_budget,
+//       amount: amount
+//     })
+//     res.json(data);
+//   } catch (error) {
+//     console.log(error)
+//   }
+
+//   if (kind_of === 'program') {
+//     const program = Program.findOne({ where: { code: kind_of_budget } });
+//     program.update({
+//       amount: amount - amount
+//     })
+//   } else {
+//     const appro = Appropriations.findOne({ where: { code: kind_of_budget } });
+//     appro.update({
+//       amount: amount - amount
+//     })
+//   }
+// }
+
+
+
 import ParentBabs from './../../models/forms/ParentBab.js';
 import Appropriations from './../../models/forms/budget/Appropriations.js';
 import Program from './../../models/forms/budget/Program.js';
+
 
 //! Get Parent Bab
 export const getParentBab = async (req, res) => {
@@ -12,38 +54,28 @@ export const getParentBab = async (req, res) => {
   }
 }
 
-
-//! Create Parent Bab
+//! Add Parent Bab
 export const createParentBab = async (req, res) => {
-
-  const name = req.body.name;
-  const desc = req.body.desc;
-  const kind_of = req.body.kind_of;
-  const kind_of_budget = req.body.kind_of_budget;
-  const amount = req.body.amount;
+  const { name, desc, kind_of, kind_of_budget, amount } = req.body;
 
   try {
     const data = await ParentBabs.create({
-      name: name,
-      desc: desc,
-      kind_of: kind_of,
-      kind_of_budget: kind_of_budget,
-      amount: amount
-    })
+      name,
+      desc,
+      kind_of,
+      kind_of_budget,
+      amount
+    });
     res.json(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 
   if (kind_of === 'program') {
-    const program = Program.findOne({ where: { code: kind_of_budget } });
-    program.update({
-      amount: amount - amount
-    })
+    const program = await Program.findOne({ where: { code: kind_of_budget } });
+    program.update({ amount: program.amount - amount });
   } else {
-    const appro = Appropriations.findOne({ where: { code: kind_of_budget } });
-    appro.update({
-      amount: amount - amount
-    })
+    const appro = await Appropriations.findOne({ where: { code: kind_of_budget } });
+    appro.update({ amount: appro.amount - amount });
   }
-}
+};
